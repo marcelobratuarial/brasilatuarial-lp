a1f433086a8cd223f454cfceedf6c52d9db6904a
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-09-23 14:07:17 -0300

    First

---
+++ partials/a1.php
@@ -0,0 +1,96 @@
+ <div  class="page position-absolute t-0 w-100" id="a1">
+		<div class="row no-gutters minh-100vh">
+      
+			 <div class="col-lg-9 order-1 order-lg-0 page-content pt-6 pt-lg-0">
+			 
+			
+			<section class="pt-5 pt-xl-7 pt-xxl-8">
+              <div class="container-fluid">
+                <div class="row justify-content-center pb-5" id="boots4-story">
+                  <div class="col-lg-10">
+                    <div class="text-center mb-5 mb-lg-6">
+                      <h2 class="fs-2 fs-sm-3"> <span class="font-weight-medium">Carro </span> Reserva</h2>
+                      <hr class="hr-ornate" />
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6 mb-4 mb-md-0">
+                        <div class="bg-holder position-relative rounded py-8 py-md-0" style="background-image:url(assets/img/infinity.jpg);">
+                        </div>
+                      </div>
+                      <div class="col-md-6 pl-3 pl-md-4">
+                        <h4>Falta de condução não será mais um problema para seus associado. De forma simples e prática</h4>
+                        <p class="lead my-3">Com o produto de Carro Reserva, em caso de sinistro, o beneficiário dispõe de um carro destinado ao uso do segurado, durante o período em que seu carro estiver imobilizado pelo sinistro acontecido, sem limite de quilometragem. </p>
+                        <p class=" mb-0">
+
+
+ </p>
+                      </div>
+                    </div>
+					
+					<div class="row">
+						
+					<div class="col-md-12 mt-3 ">
+                        
+                        <p class="lead my-3">Condições gerais </p>
+                        
+                      </div>
+					  
+					</div>
+					
+					<div class="row">
+						
+					  <div class="col-md-4 mt-5 ">
+                        
+                        <p class=" mb-0">1-Nos casos em que o usuário não atenda aos requisitos mínimos exigidos pela locadora de veículos, poderá apresentar outra pessoa, que os cumpra, para locar o veículo em seu nome;  </p>
+                      </div>
+					  
+					  <div class="col-md-4 mt-5 ">
+                        
+                        <p class=" mb-0">2-A retirada, guarda e devolução do Carro Reserva será de responsabilidade do próprio segurado ou principal condutor pessoa física, no local previamente determinado;  </p>
+                      </div>
+					  
+					  <div class="col-md-4 mt-5 ">
+                        
+                       <p class=" mb-0">3-Na retirada do Carro Reserva o associado assinará o contrato de locação com as condições e cláusulas específicas da empresa locadora; </p>
+                      </div>
+					  
+					</div>
+					
+					<div class="row">
+						
+					  <div class="col-md-6 mt-5 ">
+                        
+                        <p class=" mb-0">4-As diárias não utilizadas não serão acumulativas e será considerado o período integral. Nos casos em que o condutor queira estender o período de locação, as diárias adicionais serão de sua responsabilidade; </p>
+                      </div>
+					  
+					  <div class="col-md-6 mt-5 ">
+                        
+                        <p class=" mb-0">5-Este benefício concede a utilização do carro reserva pelo período contratado a partir da retirada da locadora. Após este prazo, a locação será cobrada no cartão de crédito do Associado conforme tabela vigente pela locadora; </p>
+                      </div>
+					  
+				
+					  
+					</div>
+					
+					
+					
+                  </div>
+                </div>
+                
+              </div>
+            </section>
+			
+				
+			 </div>
+	 
+			<!-- Titulo da Lateral -->
+			<div class="col-lg-3 col-12 t-0 order-0 order-lg-1 position-absolute position-lg-relative">
+            <div class="h-lg-100vh sticky-top py-4 sticky-area"><span class="btn-close"><img class="d-none d-lg-block times" src="assets/img/times.svg" width="25" alt=""/><img class="d-lg-none" src="assets/img/times-black.svg" width="18" alt=""/></span>
+              <div class="bg-holder" style="background-image:url(assets/img/sidebars/about.jpg);">
+              </div>
+              <h1 class="page-title">Carro Reserva</h1>
+            </div>
+			</div>
+		  
+		</div>
+	 </div>
---
+++ partials/a10.php
@@ -0,0 +1,107 @@
+ <div  class="page position-absolute t-0 w-100" id="a10">
+		<div class="row no-gutters minh-100vh">
+	 
+			 <div class="col-lg-9 order-1 order-lg-0 page-content pt-6 pt-lg-0">
+			 
+			
+			<section class="pt-5 pt-xl-7 pt-xxl-8">
+              <div class="container-fluid">
+                <div class="row justify-content-center pb-5" id="boots4-story">
+                  <div class="col-lg-10">
+                    <div class="text-center mb-5 mb-lg-6">
+                      <h2 class="fs-2 fs-sm-3"> <span class="font-weight-medium">Titulo </span> destaque</h2>
+                      <hr class="hr-ornate" />
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6 mb-4 mb-md-0">
+                        <div class="bg-holder position-relative rounded py-8 py-md-0" style="background-image:url(assets/img/infinity.jpg);">
+                        </div>
+                      </div>
+                      <div class="col-md-6 pl-3 pl-md-4">
+                        <h4>Subtitulo</h4>
+                        <p class="lead my-3">At Boots4, we believe there is a way where customers are earned rather than bought. We're obsessively passionate about it, and our mission is to help you achieve it. </p>
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+                    </div>
+					
+					<div class="row">
+						
+					<div class="col-md-12 mt-3 ">
+                        
+                        <p class="lead my-3">At Boots4, we believe there is a way where customers are earned rather than bought. We're obsessively passionate about it, and our mission is to help you achieve it. </p>
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+					</div>
+					
+					<div class="row">
+						
+					  <div class="col-md-4 mt-5 ">
+                        
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+					  <div class="col-md-4 mt-5 ">
+                        
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+					  <div class="col-md-4 mt-5 ">
+                        
+                       <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+					</div>
+					
+					<div class="row">
+						
+					  <div class="col-md-6 mt-5 ">
+                        
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+					  <div class="col-md-6 mt-5 ">
+                        
+                        <p class=" mb-0">Boots4 was founded by ThemeWagon Team in 1896. It was called the world's first bootstrap four template, and started as a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo, magni aut recusandae perspiciatis quam
+                          sed eveniet, debitis tempore natus corrupti nostrum deserunt, harum in dolor dolorem sit voluptatum iusto. recusandae perspiciatis quam sed eveniet, debitis tempore natus corrupti nostrum recusandae perspiciatis quam sed eveniet,
+                          debitis tempore. </p>
+                      </div>
+					  
+				
+					  
+					</div>
+					
+					
+					
+                  </div>
+                </div>
+                
+              </div>
+            </section>
+			
+				
+			 </div>
+	 
+			<!-- Titulo da Lateral -->
+			<div class="col-lg-3 col-12 t-0 order-0 order-lg-1 position-absolute position-lg-relative">
+            <div class="h-lg-100vh sticky-top py-4 sticky-area"><span class="btn-close"><img class="d-none d-lg-block times" src="assets/img/times.svg" width="25" alt=""/><img class="d-lg-none" src="assets/img/times-black.svg" width="18" alt=""/></span>
+              <div class="bg-holder" style="background-image:url(assets/img/sidebars/about.jpg);">
+              </div>
+              <h1 class="page-title">TELEMEDICINA</h1>
+            </div>
+			</div>
+		  
+		</div>
+	 </div>
